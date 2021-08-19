5253bb2e191f35d0fa8ecb726d2e4b7f4eeb2617
DaaronR92 <85143249+DaaronR92@users.noreply.github.com>
2021-08-18 22:08:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-let js = 'amazing';
+/*let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
 console.log(40 + 8 + 23 - 10);
 console.log('Jonas');
 console.log('23');
 
-let firstName = "Jonas";
+let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let Jonas_Matilda = "JM";
+let $function = 27;
+let person = "jonas";
+let PI = 3.1415;
+
+// Be descriptive with variable names like this.
+
+let myFirstJob = "programmer"
+let myCurrentJob = "teacher"
+
+// Not like this.
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myCurrentJob)*/
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// // console.log(typeof 'jonas');
+// // console.log(typeof 23);
+
+// javaScriptIsFun = 'YES!';
+// console.log(typeof javaScriptIsFun)
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1992;
+// console.log(year)
+// console.log(typeof year);
+
+let age = 31;
+age = 32;
+
+const birthyear = 1992;
+// birthyear = 1991;
