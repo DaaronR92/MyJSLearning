4054291fe0a14583a86a377839fda29f3b8c8540
DaaronR92 <85143249+DaaronR92@users.noreply.github.com>
2021-08-19 23:57:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -46,8 +46,57 @@ console.log(myCurrentJob)*/
 // console.log(year)
 // console.log(typeof year);
 
-let age = 31;
-age = 32;
+// let age = 31;
+// age = 32;
 
-const birthyear = 1992;
-// birthyear = 1991;
+// const birthyear = 1992;
+// // birthyear = 1991;
+// // const height;
+
+// var job = 'programmer'
+// job = 'teacher'
+
+// lastname = 'jonas'
+// console.log(lastname)
+
+// Math Operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 or 2 x 2 x 2
+
+// const firstname = 'Jonas';
+// const lastname = 'Shmetman';
+// console.log(firstname + ' ' + lastname);
+
+// // Assignment Operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1 = 101
+// x--; // x = x - 1 = 100
+// console.log(x);
+
+// // Comparison Operators
+// console.log(ageJonas > ageSarah); //Greater Than >
+// console.log(ageSarah >= 18); //Greater than or Equal to
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
