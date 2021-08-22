e69a4caa45b1f0f9c2694054252367b88c9a208f
DaaronR92 <85143249+DaaronR92@users.noreply.github.com>
2021-08-21 20:46:31 -0400

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,37 @@
+// Assignment 1
+
+
+const country = 'unitedstates';
+const continent = 'northamerica';
+let population = '3280000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = 'unitedstates';
+let language = 'english';
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// country = 'sass'
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6000000);
+console.log(population < 33000000);
+let description = "Portugal is in Europe, and 11 million of its people speak english"
+console.log(description)
+
+console.log(`Portugal is in Europe, and 11 million of its people speak english`)
+
+
+if (population >= 33000000) {
+    console.log(`The Portugal population is above average!`)
+} else {
+    console.log(`The Portugal population is below average..`)
+}
