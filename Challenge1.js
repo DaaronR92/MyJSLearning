1370763669b15f35b93ce3523737bbbc86913454
DaaronR92 <85143249+DaaronR92@users.noreply.github.com>
2021-08-25 23:29:54 -0400

    Add files via upload

--- Challenge1.js
+++ Challenge1.js
@@ -59,17 +59,53 @@
 // }
 // console.log(century);
 
-const markWeight = 95;
-const markHeight = 1.88;
-const markBmi = markWeight / markHeight ** 2;
+// const markWeight = 95;
+// const markHeight = 1.88;
+// const markBmi = markWeight / markHeight ** 2;
 
 
-const johnWeight = 85;
-const johnHeight = 1.76;
-const johnBmi = johnWeight / johnHeight ** 2;
+// const johnWeight = 85;
+// const johnHeight = 1.76;
+// const johnBmi = johnWeight / johnHeight ** 2;
 
-if (markBmi > johnBmi) {
-    console.log(`Marks is higher by ${markBmi - johnBmi}!`)
+// if (markBmi > johnBmi) {
+//     console.log(`Marks is higher by ${markBmi - johnBmi}!`)
+// } else {
+//     console.log(`Johns is higher by ${johnBmi - markBmi}!`)
+// }
+
+
+// //Type Conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+
+// console.log(String(23), 23);
+
+// // Type Coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('I am ' + '23' + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' > '10');
+
+// 5 Falsy values 0,'',null,NaN,Undefined,False
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean({}));
+console.log(Boolean('Jonas'));
+console.log(Boolean(0));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all");
 } else {
-    console.log(`Johns is higher by ${johnBmi - markBmi}!`)
+    console.log("You should get a job");
+}
+
+let height = 23;
+if (height) {
+    console.log("Yay! Height is defined!");
+} else {
+    console.log("Height is undefined.")
 }
